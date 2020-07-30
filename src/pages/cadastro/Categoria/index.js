2696f7f71add6a978be2babd7fb28665a51d3f0c
fabiosanromero <fabiosanromero@hotmail.com>
2020-07-29 23:41:08 -0300

    Intermediario Aula 3

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom'
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+
+    const [categorias, setCategorias]= useState(['Teste'])
+    const valoresIniciais = {
+        nome:'',
+        descricao:'',
+        cor:'#000',
+    }
+    const [nomeDaCategoria, setnomeDaCategoria] = useState('Valor Inicial');
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
-            <form>
+            <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+            <form onSubmit={function handleSubmit(infosDoEvento){
+                infosDoEvento.preventDefault();
+                setCategorias([
+                        ...categorias,
+                        nomeDaCategoria
+                ])
+            } }>
 
                 <label>
                     Nome da Categoria:
                     <input
                         type="text"
+                        value={nomeDaCategoria}
+                        onChange={ function funcaoHandlerQueoErroPediu(infosDoEvento){
+                            setnomeDaCategoria(infosDoEvento.target.value);
+                        }}
                     />
                 </label>
 
@@ -19,6 +37,16 @@ function CadastroCategoria() {
                     Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria,indice) => {
+                    return (
+                        <li key={`${categoria}${indice}`}>
+                            {categoria}
+                        </li>
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para home
