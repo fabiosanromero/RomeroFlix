8b375c9ef180b9a79bf4e792a06219e3822ae035
fabiosanromero <fabiosanromero@hotmail.com>
2020-07-29 21:34:58 -0300

    Final Segundo Dia

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -7,7 +7,7 @@ function CadastroVideo() {
         <PageDefault>
             <h1>Cadastro de Video</h1>
 
-            <Link to="cadastro/categoria">
+            <Link to="/cadastro/categoria">
                 Cadastrar Categoria
             </Link>
         </PageDefault>
